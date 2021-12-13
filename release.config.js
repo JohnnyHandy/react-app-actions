c6dbd7ec3362b43d09606928560d4f8b4ab5b311
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-12-13 06:00:03 -0300

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/JohnnyHandy/react-app-actions",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+    ]
+}
