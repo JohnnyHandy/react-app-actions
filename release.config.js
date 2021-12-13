0a9b579c1855d6484b94785b59bc7378a6889238
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-12-13 06:08:13 -0300

    Formatted code

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 ﻿module.exports = {
-    branches: ["main"],
-    repositoryUrl: "https://github.com/JohnnyHandy/react-app-actions",
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
-    ]
-}
+  branches: ["main"],
+  repositoryUrl: "https://github.com/JohnnyHandy/react-app-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
