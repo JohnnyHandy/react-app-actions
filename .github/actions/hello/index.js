7171721ccfa9c55c91bbce4e0f320312fdf87d17
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-12-22 05:42:05 -0300

    feat(.github): added custom action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,15 @@
+﻿const core = require('@actions/core')
+const github =require('@actions/github')
+const name = core.getInput('who-to-greet')
+
+
+try {
+    console.log(`Hello ${name}`)
+
+    const time = new Date()
+    core.setOutput('time', time.toTimeString())
+    
+    console.log(JSON.stringify(github, null, '\t'))
+} catch (e) {
+    core.setFailed(e.message)
+}
